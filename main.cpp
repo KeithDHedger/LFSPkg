0d9c0be69ba148bda45040f4f46a793a8d1e5be9
K D Hedger <kdhedger68713@gmail.com>
2018-07-31 12:06:27 +0100

    changed addresses

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 /*
  *
- * ©K. D. Hedger. Mon 12 Jun 12:02:04 BST 2017 kdhedger68713@gmail.com
+ * ©K. D. Hedger. Mon 12 Jun 12:02:04 BST 2017 keithdhedger@gmail.com
 
  * This file (main.cpp) is part of LFSMakePkg.
 
- * Projects is free software: you can redistribute it and/or modify
+ * LFSMakePkg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * at your option) any later version.
 
- * Projects is distributed in the hope that it will be useful,
+ * LFSMakePkg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
